d732959e121277b76123592bebca464490f4d041
taehwankim <wih0202@gmail.com>
2022-09-18 23:21:19 +0900

    add demo controller

---
+++ flink-demo1/src/main/kotlin/com/example/flinkdemo1/api/demo/Demo1RestController.kt
@@ -0,0 +1,14 @@
+package com.example.flinkdemo1.api.demo
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Demo1RestController {
+
+    @GetMapping( "/api/v1/demo1")
+    fun getDemo1(): ResponseEntity<String> {
+        return ResponseEntity.ok("Hello Kotlin")
+    }
+}
