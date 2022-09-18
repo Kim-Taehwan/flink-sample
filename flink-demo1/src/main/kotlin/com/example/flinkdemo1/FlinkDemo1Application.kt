16da98db5df5de9bd7cf8f58db1e3135e5f5a553
taehwankim <wih0202@gmail.com>
2022-09-18 23:07:23 +0900

    init

---
+++ flink-demo1/src/main/kotlin/com/example/flinkdemo1/FlinkDemo1Application.kt
@@ -0,0 +1,11 @@
+package com.example.flinkdemo1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlinkDemo1Application
+
+fun main(args: Array<String>) {
+    runApplication<FlinkDemo1Application>(*args)
+}
