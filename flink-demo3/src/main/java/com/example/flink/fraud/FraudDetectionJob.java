9780fe32193e56a5c82615f17edb2a86d0e879ef
taehwankim <wih0202@gmail.com>
2022-09-23 00:05:42 +0900

    alert

--- flink-demo3/src/main/java/com/example/flink/fraud/FraudDetectionJob.java
+++ flink-demo3/src/main/java/com/example/flink/fraud/FraudDetectionJob.java
@@ -14,15 +14,26 @@ public class FraudDetectionJob {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         // Creating a Source
+        /**
+         * Source 는 Kafka, RabbitMQ 등 외부 시스템에서 전송 받습니다.
+          */
         DataStream<Transaction> transactions = env
                 .addSource(new TransactionsSource())
                 .name("transactions");
 
+        /**
+         * 1. 많은 유저들의 transaction data 는 다수의 fraud detection task 를 생성해서 병렬로 처리해야 한다.
+         * 2. DataStream#keyBy 로 Transaction 을 분할(partition) 할 수 있다.
+         * 3. operator 는 보통 keyBy 뒤에 오고, context 내에서 실행됩니다.
+         */
         DataStream<Alert> alerts = transactions
                 .keyBy(Transaction::getAccountId)
                 .process(new FraudDetector())
                 .name("fraud-detector");
 
+        /**
+         * Sink 는 DataStream 을 Kafka, Cassandra, Kinesis 등 외부 시스템에 쓸 수 있습니다.
+         */
         alerts.addSink(new AlertSink())
                 .name("send-alert");
 
--- flink-demo3/src/main/java/com/example/flink/fraud/FraudDetector.java
+++ flink-demo3/src/main/java/com/example/flink/fraud/FraudDetector.java
@@ -5,6 +5,12 @@ import com.example.flink.fraud.entity.Transaction;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
 
+/**
+ * 1. FraudDetector 는 KeyedProcessFunction 을 구현합니다.
+ * 2. processElement 는 모든 트랜잭션 이벤트마다 불려집니다.
+ * 3. 첫 번째 버전에서는 모든 트랜잭션이 alert 을 생성합니다.
+ * 4. 다음 스텝에서는 좀 더 비즈니스에 의미있게 확장됩니다.
+ */
 public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert> {
     private static final long serialVersionUID = 1L;
 
