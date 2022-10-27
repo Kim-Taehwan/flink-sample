97bffcd7d336d295bb7680b917682f6e99620928
taehwankim <wih0202@gmail.com>
2022-10-27 22:52:53 +0900

    Local File

--- flink-file-source/src/main/java/com/example/flink/FileSourceJob.java
+++ flink-file-source/src/main/java/com/example/flink/FileSourceJob.java
@@ -1,10 +1,19 @@
 package com.example.flink;
 
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+import static org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.OnCheckpointRollingPolicy.build;
 
 public class FileSourceJob {
     private static Logger log = LoggerFactory.getLogger(FileSourceJob.class);
@@ -17,16 +26,32 @@ public class FileSourceJob {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
-        DataStream<String> data = env.readTextFile("file:///Users/kkomamini/dev/git/flink-sample/flink-file-source/src/main/resources/log.txt");
-        log.info("input data: {}", data.print());
+        DataStream<String> input = env.readTextFile("file:///Users/kkomamini/dev/git/flink-sample/flink-file-source/src/main/resources/log.txt");
+        log.info("input data: {}", input.print());
 
-        data.filter(new FilterFunction<String>() {
-            @Override
-            public boolean filter(String value) throws Exception {
-                log.info("data value: {}", value);
-                return value.startsWith("http://");
-            }
-        }).writeAsText("file:///Users/kkomamini/dev/git/flink-sample/flink-file-source/src/main/resources/output.txt");
+//        input.filter(new FilterFunction<String>() {
+//            @Override
+//            public boolean filter(String value) throws Exception {
+//                log.info("data value: {}", value);
+//                return value.startsWith("http://");
+//            }
+//        }).writeAsText("file:///Users/kkomamini/dev/git/flink-sample/flink-file-source/src/main/resources/output.txt");
+        Path path = Paths.get("resources", "log.txt");
+        log.info("path: {}", path.toFile().getName());
+
+        StreamingFileSink<String> sink = StreamingFileSink
+                .forRowFormat(
+                        new org.apache.flink.core.fs.Path(path.toUri()),
+                        new SimpleStringEncoder<String>("UTF-8"))
+                .withRollingPolicy(
+                        DefaultRollingPolicy.builder()
+                                .withRolloverInterval(TimeUnit.SECONDS.toMillis(15))
+                                .withInactivityInterval(TimeUnit.SECONDS.toMillis(5))
+                                .withMaxPartSize(1024)
+                                .build())
+                .build();
+
+        input.addSink(sink);
 
         env.execute("Read File");
     }
