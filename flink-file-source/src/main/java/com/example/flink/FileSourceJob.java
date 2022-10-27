98108fb16b08f24eb26b4b0375da791001eeec9d
taehwankim <wih0202@gmail.com>
2022-10-27 23:47:11 +0900

    Local File

--- flink-file-source/src/main/java/com/example/flink/FileSourceJob.java
+++ flink-file-source/src/main/java/com/example/flink/FileSourceJob.java
@@ -15,44 +15,44 @@ import java.util.concurrent.TimeUnit;
 
 import static org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.OnCheckpointRollingPolicy.build;
 
-public class FileSourceJob {
-    private static Logger log = LoggerFactory.getLogger(FileSourceJob.class);
-
-    public static void main(String[] args) throws Exception {
-        log.info("Start FileSourceJob");
-        log.debug("debug");
-        log.warn("warn");
-        log.trace("trace");
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(1);
-
-        DataStream<String> input = env.readTextFile("file:///Users/kkomamini/dev/git/flink-sample/flink-file-source/src/main/resources/log.txt");
-        log.info("input data: {}", input.print());
-
-//        input.filter(new FilterFunction<String>() {
-//            @Override
-//            public boolean filter(String value) throws Exception {
-//                log.info("data value: {}", value);
-//                return value.startsWith("http://");
-//            }
-//        }).writeAsText("file:///Users/kkomamini/dev/git/flink-sample/flink-file-source/src/main/resources/output.txt");
-        Path path = Paths.get("resources", "log.txt");
-        log.info("path: {}", path.toFile().getName());
-
-        StreamingFileSink<String> sink = StreamingFileSink
-                .forRowFormat(
-                        new org.apache.flink.core.fs.Path(path.toUri()),
-                        new SimpleStringEncoder<String>("UTF-8"))
-                .withRollingPolicy(
-                        DefaultRollingPolicy.builder()
-                                .withRolloverInterval(TimeUnit.SECONDS.toMillis(15))
-                                .withInactivityInterval(TimeUnit.SECONDS.toMillis(5))
-                                .withMaxPartSize(1024)
-                                .build())
-                .build();
-
-        input.addSink(sink);
-
-        env.execute("Read File");
-    }
-}
+//public class FileSourceJob {
+//    private static Logger log = LoggerFactory.getLogger(FileSourceJob.class);
+//
+//    public static void main(String[] args) throws Exception {
+//        log.info("Start FileSourceJob");
+//        log.debug("debug");
+//        log.warn("warn");
+//        log.trace("trace");
+//        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+//        env.setParallelism(1);
+//
+//        DataStream<String> input = env.readTextFile("file:///Users/kkomamini/dev/git/flink-sample/flink-file-source/src/main/resources/log.txt");
+//        log.info("input data: {}", input.print());
+//
+////        input.filter(new FilterFunction<String>() {
+////            @Override
+////            public boolean filter(String value) throws Exception {
+////                log.info("data value: {}", value);
+////                return value.startsWith("http://");
+////            }
+////        }).writeAsText("file:///Users/kkomamini/dev/git/flink-sample/flink-file-source/src/main/resources/output.txt");
+//        Path path = Paths.get("resources", "log.txt");
+//        log.info("path: {}", path.toFile().getName());
+//
+//        StreamingFileSink<String> sink = StreamingFileSink
+//                .forRowFormat(
+//                        new org.apache.flink.core.fs.Path(path.toUri()),
+//                        new SimpleStringEncoder<String>("UTF-8"))
+//                .withRollingPolicy(
+//                        DefaultRollingPolicy.builder()
+//                                .withRolloverInterval(TimeUnit.SECONDS.toMillis(15))
+//                                .withInactivityInterval(TimeUnit.SECONDS.toMillis(5))
+//                                .withMaxPartSize(1024)
+//                                .build())
+//                .build();
+//
+//        input.addSink(sink);
+//
+//        env.execute("Read File");
+//    }
+//}
